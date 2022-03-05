90109a587b2cbc34c8d58c4ad2b9ea2b56a180f5
Paul Hoffmann <paul.hoffmann@7p-group.com>
2022-03-05 13:46:52 +0100

    Add docker

---
+++ docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Set up the repository
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable repository
+echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+	    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
