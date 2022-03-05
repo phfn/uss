5463bc03bd7a17f2f78ebf0df8f00e76e4c7b32e
Paul Hoffmann <paul.hoffmann@7p-group.com>
2022-03-05 22:57:07 +0100

    Update adduser

--- adduser.py
+++ adduser.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 from argparse import ArgumentParser
 from os import system, makedirs
+from getpass import getpass
 
 def parse_args():
     parser = ArgumentParser()
@@ -8,9 +9,8 @@ def parse_args():
     parser.add_argument("users", nargs="+", help="Users to add")
     return parser.parse_args()
 
-def add_ssh_key(user):
-    ssh_key = input("Please past the public ssh_key:\n")
-    makedirs(f"/home/{user}/.ssh")
+def add_ssh_key(user, ssh_key):
+    makedirs(f"/home/{user}/.ssh", exist_ok=True)
     with open(f"/home/{user}/.ssh/authorized_keys", "w+") as file:
         file.write("\n")
         file.write(ssh_key)
@@ -21,11 +21,14 @@ def add_ssh_key(user):
 def run(cmd: str):
     print(cmd)
     return system(cmd)
+
 def add_user(user: str):
     groups = "docker,sudo"
-    cmd(f"sudo useradd --create-home --groups {groups}")
-    cmd(f"passwd {user}")
-    add_ssh_key(user)
+    password = getpass("Please enter a password:\n")
+    ssh_key = input("Please past the public ssh_key:\n")
+    run(f"sudo useradd --create-home --groups {groups}")
+    run(f"passwd {user}")
+    add_ssh_key(user, ssh_key)
 
 
 def main():
