8213fead5b7ca7648f4f56b77ccb683280267eed
Paul Hoffmann <paul.hoffmann@7p-group.com>
2022-03-06 19:26:33 +0100

    REmove functions

--- adduser.py
+++ adduser.py
@@ -5,49 +5,27 @@ from getpass import getpass
 from subprocess import Popen
 from crypt import crypt
 
-def is_root():
-    return getuid()==0
-
-def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument("users", nargs="+", help="Users to add")
-    return parser.parse_args()
-
-def add_ssh_key(user, ssh_key):
-    makedirs(f"/home/{user}/.ssh", exist_ok=True)
-    with open(f"/home/{user}/.ssh/authorized_keys", "w+") as file:
-        file.write("\n")
-        file.write(ssh_key)
-
-
-def run(cmd: str, should_print = True):
-    if print:
-        print(cmd)
+def run(cmd: str):
+    print(cmd)
     return system(cmd)
 
+if getuid()!=0:
+    print("Please use this script as root")
+    exit(1)
+parser = ArgumentParser()
+parser.add_argument("users", nargs="+", help="Users to add")
+args = parser.parse_args()
 
-
-
-def add_user(user: str):
+for user in args.users:
     print(f"Adding user {user}")
     groups = "docker,sudo"
     password = getpass("Please enter a password: ")
     ssh_key = input("Please past the public ssh_key:\n")
+
     run(f"useradd --create-home --groups {groups} --shell /bin/bash {user}")
     run(f"echo \"{user}:{password}\" | chpasswd", should_print = False)
-    add_ssh_key(user, ssh_key)
 
-
-def main():
-    args = parse_args()
-
-    if not is_root():
-        print("Please use this script as root")
-        exit(1)
-
-    for user in args.users:
-        add_user(user)
-
-
-if __name__ == "__main__":
-    main()
+    makedirs(f"/home/{user}/.ssh", exist_ok=True)
+    with open(f"/home/{user}/.ssh/authorized_keys", "w+") as file:
+        file.write("\n")
+        file.write(ssh_key)
